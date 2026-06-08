cb75ce9b084ef713a2727b4cafeeabc662c61798
Shahd <shahad9mubarak@gmail.com>
2026-06-08 10:07:03 +0400

    create employees class

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK38/EmployeePOSTDemo.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK38/EmployeePOSTDemo.java
@@ -1,4 +1,8 @@
 package com.example.Springboot.APIs8.TASK38;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmployeePOSTDemo {
+    private static List<Employee> employees = new ArrayList<>();
 }
