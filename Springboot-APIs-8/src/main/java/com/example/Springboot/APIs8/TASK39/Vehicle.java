42330d68428dd6e75055f8e3bbd652656a791846
Shahd <shahad9mubarak@gmail.com>
2026-06-08 10:36:23 +0400

    Add Constructor

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK39/Vehicle.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK39/Vehicle.java
@@ -1,5 +1,10 @@
 package com.example.Springboot.APIs8.TASK39;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class Vehicle {
     private String vehicleId;
     private int vehicleModel;
