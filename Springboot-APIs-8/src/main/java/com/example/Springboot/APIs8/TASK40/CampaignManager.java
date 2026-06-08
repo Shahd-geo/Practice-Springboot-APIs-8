c8da39c7e3a8ae294fed629c90fbe01e4de76fb8
Shahd <shahad9mubarak@gmail.com>
2026-06-08 11:20:11 +0400

    // Initialize with sample campaign

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK40/CampaignManager.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK40/CampaignManager.java
@@ -5,4 +5,10 @@ import java.util.List;
 
 public class CampaignManager {
     private List<Campaign> campaigns = new ArrayList<>();
+    // Initialize with sample campaigns
+    public CampaignManager() {
+        campaigns.add(new Campaign("C101", "Summer Sale", "Instagram", 500));
+        campaigns.add(new Campaign("C102", "Black Friday", "Google Ads", 1000));
+        campaigns.add(new Campaign("C103", "Email Promo", "Email", 300));
+    }
 }
