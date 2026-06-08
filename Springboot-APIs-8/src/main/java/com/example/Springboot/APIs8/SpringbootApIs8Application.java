bb99c9aa75a9da766634fd5c8f3dfed690a81048
Shahd <shahad9mubarak@gmail.com>
2026-06-08 09:53:36 +0400

    create  Employee class

---
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/SpringbootApIs8Application.java
@@ -0,0 +1,13 @@
+package com.example.Springboot.APIs8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApIs8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApIs8Application.class, args);
+	}
+
+}
---
+++ Springboot-APIs-8/src/test/java/com/example/Springboot/APIs8/SpringbootApIs8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springboot.APIs8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApIs8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
