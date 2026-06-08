938eb82fbb2dcfcfdc32885fad42182739bd38ef
Shahd <shahad9mubarak@gmail.com>
2026-06-08 11:16:20 +0400

    Add Getter Methods

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK40/Campaign.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK40/Campaign.java
@@ -1,10 +1,12 @@
 package com.example.Springboot.APIs8.TASK40;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 
 public class Campaign {
     private String campaignId;
