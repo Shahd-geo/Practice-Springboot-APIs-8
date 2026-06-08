9b3d8eb22db9c9fd104486445c8eec8073a53ff1
Shahd <shahad9mubarak@gmail.com>
2026-06-08 11:05:33 +0400

    Create a Vehicle Collection

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK39/VehicleManager.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK39/VehicleManager.java
@@ -6,13 +6,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class VehicleManager {
-    private  List<Vehicle> vehicle = new ArrayList<>();
+    private  List<Vehicle> vehicles = new ArrayList<>();
     public VehicleManager(){
         //  Store existing employees
-        vehicle.add(new Vehicle("V101", "Toyota Corolla",  20 );
-        vehicle.add(new Vehicle("V102", "Nissan Sunny", 18 ));
-        vehicle.add(new Vehicle("V103", "Hyundai Elantra", 22 ));
+        vehicles.add(new Vehicle("V101", "Toyota Corolla",  20 ));
+        vehicles.add(new Vehicle("V102", "Nissan Sunny", 18 ));
+        vehicles.add(new Vehicle("V103", "Hyundai Elantra", 22 ));
     }
         // Add Create Method
+    public String addVehicle(Vehicle vehicle) {
+        for (Vehicle V : vehicles) {
+            if (V.getVehicleId().equals(vehicle.getVehicleId())) {
+                return "Vehicle ID already exists , No vehicle was added";
+            }
+
+        }
+        vehicles.add(vehicle);
+        System.out.println("Vehicle Added Successfully");
+        return ("Vehicle ID: " + vehicle.getVehicleId())+"Vehicle Model: " + vehicle.getVehicleModel()+ "Rental Price Per Day: " + vehicle.getRentalPricePerDay() + " OMR" + "Status:Created";
+    }
+
+
 
 }
