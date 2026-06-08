ed1707b917033d00ec3618484bbf197bed1daa33
Shahd <shahad9mubarak@gmail.com>
2026-06-08 11:15:25 +0400

    add fields

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK40/Campaign.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK40/Campaign.java
@@ -1,4 +1,8 @@
 package com.example.Springboot.APIs8.TASK40;
 
 public class Campaign {
+    private String campaignId;
+    private String campaignName;
+    private String platform;
+    private double budget;
 }
