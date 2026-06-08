a8b196fd9b160a15faca49d82b20a4a85a01bcb9
Shahd <shahad9mubarak@gmail.com>
2026-06-08 10:37:04 +0400

    . Add Getter Methods USING LOMBOK

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK39/Vehicle.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK39/Vehicle.java
@@ -1,10 +1,12 @@
 package com.example.Springboot.APIs8.TASK39;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Vehicle {
     private String vehicleId;
     private int vehicleModel;
