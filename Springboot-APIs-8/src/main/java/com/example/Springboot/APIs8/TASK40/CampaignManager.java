ea11c05aec1f359cc5ae427a5dbe8826860193e5
Shahd <shahad9mubarak@gmail.com>
2026-06-08 11:19:30 +0400

    create list

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK40/CampaignManager.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK40/CampaignManager.java
@@ -1,4 +1,8 @@
 package com.example.Springboot.APIs8.TASK40;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CampaignManager {
+    private List<Campaign> campaigns = new ArrayList<>();
 }
