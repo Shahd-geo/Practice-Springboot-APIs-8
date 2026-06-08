c412d1708cba0e40f8324e77c4251e33b958749b
Shahd <shahad9mubarak@gmail.com>
2026-06-08 10:18:34 +0400

    Display Existing Employees

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK38/EmployeePOSTDemo.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK38/EmployeePOSTDemo.java
@@ -10,4 +10,10 @@ public class EmployeePOSTDemo {
         employees.add(new Employee("E102", "Sara", "HR"));
         employees.add(new Employee("E102", "Ahmed", "Finance"));
         employees.add(new Employee("E103", "John", "Marketing"));
-}
+        // Display Existing Employees
+        System.out.println("=== Existing Employees ===");
+        for (Employee employee : employees) {
+            System.out.println(employee);
+
+    }
+
