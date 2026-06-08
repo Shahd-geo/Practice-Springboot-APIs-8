de229ec1a436dea0b0bbddc1630aa5d37992ce01
Shahd <shahad9mubarak@gmail.com>
2026-06-08 10:01:07 +0400

    add getter and setter

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK38/Employee.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/TASK38/Employee.java
@@ -11,9 +11,33 @@ public class Employee {
         this.department = department;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
     @Override
     public String toString() {
         return id + " → " + name + " → " + department;
     }
 }
-}
+
