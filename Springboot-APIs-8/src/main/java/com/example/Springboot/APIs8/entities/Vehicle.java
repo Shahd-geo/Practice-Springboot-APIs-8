8b6bb91fa46a2bb90aae06709852f1fa7dae8732
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-09 10:18:10 +0400

    NAME TABLE OF FRIST ENTITY

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/entities/Vehicle.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/entities/Vehicle.java
@@ -1,5 +1,6 @@
 package com.example.Springboot.APIs8.entities;
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
 public class Vehicle {
     private String vehicleId;
     private String vehicleModel;
