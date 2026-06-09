143109e25284ffc12415ddb668dbcdb0ba2cea57
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-09 10:54:37 +0400

    ؤCREATE EmployeeController

---
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/controllers/EmployeeController.java
@@ -0,0 +1,4 @@
+package com.example.Springboot.APIs8.controllers;
+
+public class EmployeeController {
+}
--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/services/EmployeeService.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/services/EmployeeService.java
@@ -1,11 +1,22 @@
 package com.example.Springboot.APIs8.services;
 
+import com.example.Springboot.APIs8.entities.Employee;
 import com.example.Springboot.APIs8.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
 }
+
