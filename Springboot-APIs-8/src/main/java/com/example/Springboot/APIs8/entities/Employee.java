157ba10c4d8f9a7291b2d4c7d6b915f8a64a6df8
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-09 10:16:09 +0400

    DETERMINE THE PK

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/entities/Employee.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/entities/Employee.java
@@ -1,6 +1,9 @@
 package com.example.Springboot.APIs8.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,6 +11,8 @@ import lombok.Data;
 @Data
 @Entity
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
     private String name;
     private String department;
