1827859d2273eacd1d230f5e3a693d3026ef3359
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-09 10:56:24 +0400

    EDIT IN EmployeeController (ADD+GITALL)

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/controllers/EmployeeController.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/controllers/EmployeeController.java
@@ -1,4 +1,28 @@
 package com.example.Springboot.APIs8.controllers;
 
+import com.example.Springboot.APIs8.entities.Employee;
+import com.example.Springboot.APIs8.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class EmployeeController {
+    @Autowired
+    EmployeeService employeeService;
+
+    @PostMapping("/add")
+    public Employee addEmployee(@RequestBody Employee employee){
+        return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping("/getAll")
+    public List<Employee> getAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
 }
+
