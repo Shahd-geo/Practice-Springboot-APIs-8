702f78a8c5eef61f395eecccd7d41bb0728cc291
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-11 09:34:31 +0400

    ADD RequestMapping

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/services/VehicleService.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/services/VehicleService.java
@@ -13,11 +13,47 @@ public class VehicleService {
         VehicleRepository vehicleRepository;
 
         public Vehicle saveVehicle(Vehicle vehicle){
+
             return vehicleRepository.save(vehicle);
         }
 
         public List<Vehicle> getAllVehicles(){
+
             return vehicleRepository.findAll();
         }
+    // GET BY ID
+    public Vehicle getVehicleById(Integer id){
+        return vehicleRepository.findById(id).orElse(null);
     }
+
+    // UPDATE
+    public Vehicle updateVehicle(Integer id, Vehicle updatedVehicle){
+
+        Vehicle existingVehicle = vehicleRepository.findById(id).orElse(null);
+
+        if(existingVehicle == null){
+            return null;
+        }
+
+        existingVehicle.setVehicleModel(updatedVehicle.getVehicleModel());
+        existingVehicle.setRentalPricePerDay(updatedVehicle.getRentalPricePerDay());
+
+        return vehicleRepository.save(existingVehicle);
+    }
+
+    // DELETE
+    public String deleteVehicle(Integer id){
+
+        Vehicle vehicle = vehicleRepository.findById(id).orElse(null);
+
+        if(vehicle == null){
+            return "Vehicle not found";
+        }
+
+        vehicleRepository.deleteById(id);
+
+        return "Vehicle deleted successfully";
+    }
+}
+
 
