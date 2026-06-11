2b8ee9366d4594a7b7c1e58b7ceb06cab68d48ce
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-11 08:40:27 +0400

    ADD Validation Dependency

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/entities/Campaign.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/entities/Campaign.java
@@ -1,6 +1,9 @@
 package com.example.Springboot.APIs8.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,8 +17,14 @@ public class Campaign {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer campaignId;
+    @NotBlank(message = "Campaign name cannot be empty")
     private String campaignName;
+
+    @NotBlank(message = "Platform cannot be empty")
     private String platform;
+
+    @NotNull(message = "Budget is required")
+    @Positive(message = "Budget must be greater than 0")
     private double budget;
     @ManyToOne
     private Employee employee;
