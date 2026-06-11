29378d27b855b69bae2ba17189093dccde735206
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-11 09:02:12 +0400

    ADD Validation in VEHICLE  class

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/controllers/CampaignController.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/controllers/CampaignController.java
@@ -4,10 +4,7 @@ import com.example.Springboot.APIs8.entities.Campaign;
 import com.example.Springboot.APIs8.services.CampaignService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +20,19 @@ public class CampaignController {
 
     @GetMapping("/campaign/getAll")
     public List<Campaign> getAllCampaigns(){
+
         return campaignService.getAllCampaigns();
+    }
+    @GetMapping("/campaign/{id}")
+    public Campaign getCampaignById(@PathVariable Integer id){
+        return campaignService.getCampaignById(id);
+    }
+    @PutMapping("/campaign/update/{id}")
+    public Campaign updateCampaign(@PathVariable Integer id, @Valid @RequestBody Campaign campaign){
+        return campaignService.updateCampaign(id, campaign);
+    }
+    @DeleteMapping("/campaign/delete/{id}")
+    public String deleteCampaign(@PathVariable Integer id){
+        return campaignService.deleteCampaign(id);
     }
 }
