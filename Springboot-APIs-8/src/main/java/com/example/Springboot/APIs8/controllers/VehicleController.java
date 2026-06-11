c7dc7735e0a0b6413da19f7820eea2f4af80e3f0
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-11 09:38:07 +0400

    ADD @Valid IN CONTROLLER

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/controllers/VehicleController.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/controllers/VehicleController.java
@@ -2,27 +2,46 @@ package com.example.Springboot.APIs8.controllers;
 
 import com.example.Springboot.APIs8.entities.Vehicle;
 import com.example.Springboot.APIs8.services.VehicleService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/vehicle")
 public class VehicleController {
     @Autowired
     VehicleService vehicleService;
 
-    @PostMapping("/vehicle/add")
-    public Vehicle addVehicle(@RequestBody Vehicle vehicle){
+    @PostMapping("/add")
+    public Vehicle addVehicle(@Valid @RequestBody Vehicle vehicle){
         return vehicleService.saveVehicle(vehicle);
     }
 
-    @GetMapping("/vehicle/getAll")
+    @GetMapping("/getAll")
     public List<Vehicle> getAllVehicles(){
+
         return vehicleService.getAllVehicles();
     }
+
+    @GetMapping("/{id}")
+    public Vehicle getVehicleById(@Valid@PathVariable Integer id){
+        return vehicleService.getVehicleById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public Vehicle updateVehicle(@Valid@PathVariable Integer id, @RequestBody Vehicle vehicle){
+
+        return vehicleService.updateVehicle(id, vehicle);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteVehicle(@Valid@PathVariable Integer id){
+
+        return vehicleService.deleteVehicle(id);
+    }
 }
+
+
 
