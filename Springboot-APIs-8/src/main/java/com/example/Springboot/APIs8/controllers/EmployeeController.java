c81083de34661d94efaef35cb2271015a4679146
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-11 09:24:35 +0400

    ADD Validation in VEHICLE  class

--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/controllers/EmployeeController.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/controllers/EmployeeController.java
@@ -4,10 +4,7 @@ import com.example.Springboot.APIs8.entities.Employee;
 import com.example.Springboot.APIs8.services.EmployeeService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +21,20 @@ public class EmployeeController {
     @GetMapping("/getAll")
     public List<Employee> getAllEmployees(){
         return employeeService.getAllEmployees();
+    }
+    @GetMapping("/employee/{id}")
+    public Employee getEmployeeById(@PathVariable Integer id){
+        return employeeService.getEmployeeById(id);
+    }
+    @PutMapping("/employee/update/{id}")
+    public Employee updateEmployee(@PathVariable Integer id, @Valid @RequestBody Employee employee){
+
+        return employeeService.updateEmployee(id, employee);
+    }
+    @DeleteMapping("/employee/delete/{id}")
+    public String deleteEmployee(@PathVariable Integer id){
+
+        return employeeService.deleteEmployee(id);
     }
 }
 
--- Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/services/EmployeeService.java
+++ Springboot-APIs-8/src/main/java/com/example/Springboot/APIs8/services/EmployeeService.java
@@ -12,13 +12,48 @@ import java.util.List;
 public class EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
+    //ADD
     public Employee saveEmployee( Employee employee) {
 
         return employeeRepository.save(employee);
     }
+    //GET ALL - FIND ALL
 
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+    // GET BY ID
+    public Employee getEmployeeById(Integer id){
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    // UPDATE
+    public Employee updateEmployee(Integer id , Employee updatedEmployee){
+
+        Employee existingEmployee = employeeRepository.findById(id).orElse(null);
+
+        if(existingEmployee == null){
+            return null;
+        }
+
+        existingEmployee.setName(updatedEmployee.getName());
+        existingEmployee.setDepartment(updatedEmployee.getDepartment());
+
+        return employeeRepository.save(existingEmployee);
+    }
+
+    // DELETE
+    public String deleteEmployee(Integer id){
+
+        Employee employee = employeeRepository.findById(id).orElse(null);
+
+        if(employee == null){
+            return "Employee not found";
+        }
+
+        employeeRepository.deleteById(id);
+
+        return "Employee deleted successfully";
     }
 }
 
